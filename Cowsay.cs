2def9c5ddc8f818af449b573812bc1f800701f5c
harknoel <bontilao.harknoel@gmail.com>
2024-06-18 19:44:25 +0800

    Refactor code

--- Cowsay.cs
+++ Cowsay.cs
@@ -1,11 +1,40 @@
+using System.Diagnostics;
 public class Cowsay
 {
     public event EventHandler<CowsayEventArgs>? CowsayEvent;
 
     public void InvokeCowsay(string? input)
     {
-        CowsayEventArgs cowsayEventArgs = new CowsayEventArgs();
-        cowsayEventArgs.Say(input);
+        string output = Say(input);  // Call the Say method to get the output
+        CowsayEventArgs cowsayEventArgs = new CowsayEventArgs { Output = output };
         CowsayEvent?.Invoke(this, cowsayEventArgs);
+    }
+
+    private string Say(string? input)
+    {
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = "/bin/zsh",
+            Arguments = $"-c \"cowsay '{input}'\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using Process process = new Process();
+        process.StartInfo = psi;
+        process.Start();
+        string output = process.StandardOutput.ReadToEnd();
+
+        string error = process.StandardError.ReadToEnd();
+
+        if (!string.IsNullOrEmpty(error))
+        {
+            Console.WriteLine($"Error: {error}");
+        }
+
+        process.WaitForExit();
+        return output;
     }
 }
--- CowsayEventArgs.cs
+++ CowsayEventArgs.cs
@@ -1,33 +1,4 @@
-using System.Diagnostics;
 public class CowsayEventArgs : EventArgs
 {
-    public string? Output { get; set;}
-
-    public void Say(string? Input)
-    {
-        ProcessStartInfo psi = new ProcessStartInfo
-        {
-            FileName = "/bin/zsh",
-            Arguments = $"-c \"cowsay '{Input}'\"",
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
-
-        using Process process = new();
-        process.StartInfo = psi;
-        process.Start();
-        string output = process.StandardOutput.ReadToEnd();
-        Output = output;
-
-        string error = process.StandardError.ReadToEnd();
-
-        if (!string.IsNullOrEmpty(error))
-        {
-            Console.WriteLine($"Error: {error}");
-        }
-
-        process.WaitForExit();
-    }
+    public string? Output { get; set; }
 }
